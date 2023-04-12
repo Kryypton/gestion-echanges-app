cded040e109c27dfc36d55a38436d9ead8ec5f39
Quentin Moutté <q.moutte@icloud.com>
2023-04-12 14:29:30 +0200

    avancement classe

--- src/Criterion.java
+++ src/Criterion.java
@@ -1,4 +1,4 @@
-
+import org.w3c.dom.css.CSSValueList;
 
 public class Criterion{
 
@@ -14,12 +14,15 @@ public class Criterion{
     public boolean isValid(){
 
         if(this.label.getType() == 'B'){
-
             return this.value.equals("yes") || this.value.equals("no");
-
-           }
-               return true;
-            }
+        }else if(this.label.getType() == 'T'){
+            return //faire equals avec obj string
+        }else if(this.label.getType() == 'N'){
+            return //faire equals avec obj string
+        }else if(this.label.getType() == 'D'){
+            return //faire equals avec obj string
+        }
+    }
 
 
     public CriterionName getLabel() {
@@ -30,9 +33,23 @@ public class Criterion{
         return value;
     }
 
-    
-                
-        
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Criterion other = (Criterion) obj;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        if (label != other.label)
+            return false;
+        return true;
+    }
 }
-
