36dc66d58eba3de59bfc7cfd7723f8532ecebf45
Nathan Dorny <nathan.dorny.etu@univ-lille.fr>
2023-05-21 00:51:32 +0200

    1/2 isvalid

--- src/Criterion.java
+++ src/Criterion.java
@@ -31,24 +31,46 @@ public class Criterion{
      * @return Si le critere n'est pas valide renvoie une exception
      */
 
-    public void isValid() throws CriterionTypeException {
+    public boolean isValid() throws CriterionTypeException {
 
         if (label == null) {
             throw new CriterionTypeException("Le label du critère est null");
         }
         if (label.getType() == 'B' && !this.equals("yes") && !this.equals("no")) {
-            return new CriterionTypeException("Le critère qui représente un booléen doit être égal à yes ou no");
+            throw new CriterionTypeException("Le critère qui représente un booléen doit être égal à yes ou no");
         } else if (label.getType() == 'N' && !isNumeric(value)) {
-                return new CriterionTypeException("Le critère qui représente un nombre doit être un nombre");
+            throw new CriterionTypeException("Le critère qui représente un nombre doit être un nombre");
         } else if (label.getType() == 'D') {
                 String[] date = this.value.split("/");
                 if(!Criterion.isNumeric(date[0]) || !Criterion.isNumeric(date[1]) || !Criterion.isNumeric(date[2])){
-                    return new CriterionTypeException("Le critère qui représente une date doit être une date");
+                    throw new CriterionTypeException("Le critère qui représente une date doit être une date");
                 }
         } else if (label.getType() == 'T' && !isText(value)) {
-                return new CriterionTypeException("Le critère qui représente du texte doit être du texte");
+            throw new CriterionTypeException("Le critère qui représente du texte doit être du texte");
+            } else {
+            return true;
             }
+        return false;
         }
+
+    
+    private static boolean isNumeric(String string) {
+        try {
+            Integer.parseInt(string);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    
+    private static boolean isText(String string){
+        try {
+            Integer.parseInt(string);
+            return false;
+        } catch (NumberFormatException e) {
+            return true;
+        }
+    }
 
     /**
      * Cette méthode permet de récupérer le label du critére
--- src/CriterionTypeException.java
+++ src/CriterionTypeException.java
@@ -1,4 +1,4 @@
-package src;
+
 
 public class CriterionTypeException extends Exception {
 
