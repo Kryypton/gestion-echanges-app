821898fba7036301f22a8dcdbc0882258fc1b29e
Nathan Dorny <nathan.dorny.etu@univ-lille.fr>
2023-06-07 12:46:53 +0200

    hitory (probleme)

--- src/History.java
+++ src/History.java
@@ -5,55 +5,66 @@ import java.util.*;
 import fr.ulille.but.sae2_02.graphes.Arete;
 
 public class History implements Serializable {
-    private Map<Integer, Map<Teenager , Teenager>> affectationsHistory ;
+    private Map<Integer, Map<Teenager , Teenager>> affectationsHistory;
 
     public History() {
         this.affectationsHistory = new HashMap<Integer, Map<Teenager , Teenager>>();
     }
 
     // Création de la hashmap de Teenagers et les arretes qui vont avec entre les Teenager.
-    public History(List<fr.ulille.but.sae2_02.graphes.Arete<Teenager>> aretes){
-        this();
-        for(Arete<Teenager> arete : aretes){
-            this.affectations(arete.getExtremite1() , arete.getExtremite2());
+    public History(Map<Integer, Arete<Teenager>> aretes){
+        this.affectationsHistory = new HashMap<Integer, Map<Teenager , Teenager>>();
+        for(Map.Entry<Integer, Arete<Teenager>> entry : aretes.entrySet()){
+            this.affectationsHistory.put(entry.getKey() , new HashMap<Teenager, Teenager>());
         }
     }
 
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<Integer, Map<Teenager , Teenager>> entry : affectationsHistory.entrySet()) {
+            sb.append("Année : ").append(entry.getKey()).append("\n");
+            for (Map.Entry<Teenager, Teenager> entry2 : entry.getValue().entrySet()) {
+                sb.append("\t").append(entry2.getKey().getName()).append(" est chez ").append(entry2.getValue().getName()).append("\n");
+            }
+        }
+        return sb.toString();
+    }
+    
+
+    /*
     // Enleve le teenager courant qui est avec un autre.
     public void desaffectations(Teenager t){
         this.affectationsHistory.remove(t);
-    }
+    }*/
 
     // Affecte 2 Teenagers
-    public void affectations(Teenager t1 , Teenager t2){
-        this.affectationsHistory.put(t1 , t2);
+    public void affectations(Teenager t1 , Teenager t2, int year){
+        Map<Teenager, Teenager> affectation = new HashMap<Teenager, Teenager>();
+        affectation.put(t1 , t2);
+        this.affectationsHistory.put(year, affectation);
     }
 
     // Retourne le Teenager associer au Teenager courant.
-    public Teenager get( Teenager t){
-        return this.affectationsHistory.get();
+    public Teenager get(Teenager t, int year){
+        if (estAffecter(t, year)) {
+            return this.affectationsHistory.get(year).get(t);
+        }
+        return null;
     }
 
     // renvoie true si un teenager est affecter a un teenager.
-    public boolean estAffecter(Teenager t){
-        if(this.affectationsHistory.containsKey(t)){
+    public boolean estAffecter(Teenager t, int year){
+        if(this.affectationsHistory.containsKey(year) && this.affectationsHistory.get(year).containsKey(t)){
             return true;
         }
         return false;
     }
-
     
     public boolean estAffecter(Teenager t1 , Teenager t2){
         if(this.affectationsHistory.containsKey(t1) && this.affectationsHistory.get(t1) == t2){
             return true;
         }
         return false;
-    }
-
-
-    // retourne tout les associations 
-    public Map<Teenager, Teenager> getAssociations() {
-        return this.affectationsHistory ;
     }
 
 
@@ -84,7 +95,7 @@ public class History implements Serializable {
         try {
             FileInputStream fileInputStream = new FileInputStream(filename);
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-            Map<Teenager, Teenager> history = (Map<Teenager, Teenager>) objectInputStream.readObject();
+            Map<Integer, Map<Teenager , Teenager>>  history = (Map<Integer, Map<Teenager , Teenager>>) objectInputStream.readObject();
             affectationsHistory.putAll(history); // Utiliser putAll pour copier les éléments dans affectationsHistory
             objectInputStream.close();
             fileInputStream.close();
@@ -96,9 +107,10 @@ public class History implements Serializable {
 
 
     // Methode qui permet de réevaluer le poids d'un arrete en fonction de l'historique des Teenagers.
-    public int historyTeenager(Teenager host , Teenager visitor){
-        if(estAffecter(host)){
-            if(this.get(host).equals(visitor)){
+    public int historyTeenager(Teenager host , Teenager visitor, int year){
+
+        if (estAffecter(host, year)) {
+            if (this.get(host, year).equals(visitor)){
                 if(host.getCriterion(CriterionName.HISTORY).equalsIgnoreCase("meme") || visitor.getCriterion(CriterionName.HISTORY).equalsIgnoreCase("meme")){
                     return -100;
                 }
@@ -106,8 +118,8 @@ public class History implements Serializable {
                     return 50;
                 }
             }
-        }else if(estAffecter(visitor)){
-            if(this.get(visitor).equals(host)){
+        } else if(estAffecter(visitor, year)){
+            if(this.get(visitor, year).equals(host)){
                 if(host.getCriterion(CriterionName.HISTORY).equalsIgnoreCase("meme") || visitor.getCriterion(CriterionName.HISTORY).equalsIgnoreCase("meme")){
                     return -100;
                 }
@@ -119,8 +131,6 @@ public class History implements Serializable {
         return 0;
     }
 
-
-
     public static void main(String[] args) {
         Teenager teenager1 = new Teenager(1, "teen1", "A", "M", LocalDate.of(2000, 5, 10), Country.FRANCE);
         Teenager teenager2 = new Teenager(2, "teen2", "B", "F", LocalDate.of(2001, 8, 15), Country.GERMANY);
@@ -128,10 +138,13 @@ public class History implements Serializable {
         Teenager teenager4 = new Teenager(4, "teen4", "C", "F", LocalDate.of(2002, 10, 20), Country.SPAIN);
 
         History history = new History();
-        history.affectations(teenager1 , teenager2);
-        history.affectations(teenager3 , teenager4);
+        history.affectations(teenager1 , teenager2, 0);
+        history.affectations(teenager3 , teenager4, 0);
+        
+        System.out.println(history);
         // sauvegarde de l'historique dans un fichier
         String filename = "./res/historique.ser";
+        
         history.saveHistory(filename);
 
         // Chargement de l'historique à partir d'un fichier
@@ -139,9 +152,11 @@ public class History implements Serializable {
         loadedHistory.loadHistory(filename);
 
         // Affichage 
-        System.out.println("historique chargé :");
-        for (Teenager t : loadedHistory.getAssociations().keySet()) {
-            System.out.println(t.getName() + " -> " + loadedHistory.getAssociations().get(t).getName());
+        for (Map.Entry<Integer, Map<Teenager , Teenager>> entry : loadedHistory.affectationsHistory.entrySet()) {
+            System.out.println("year : " + entry.getKey());
+            for (Map.Entry<Teenager , Teenager> entry2 : entry.getValue().entrySet()) {
+                System.out.println("host : " + entry2.getKey().getName() + " visitor : " + entry2.getValue().getName() + "année : " + entry.getKey());
+            }
         }
     }
 }
